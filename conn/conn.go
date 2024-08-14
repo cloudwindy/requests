22e230544ed64621c2e1136de66a602cac51da0c
cloudwindy <49033775+cloudwindy@users.noreply.github.com>
2024-08-14 23:27:50 +0800

    Add transport wrapper.

---
+++ conn/conn.go
@@ -0,0 +1,124 @@
+package conn
+
+import (
+	"context"
+	"crypto/tls"
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
+	utls "github.com/refraction-networking/utls"
+	"golang.org/x/net/http2"
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type Conn interface {
+	RoundTrip(r *http.Request) (resp *http.Response, err error)
+}
+
+type conn struct {
+	transport http.RoundTripper
+}
+
+type Config struct {
+	Proxy     string
+	DisableH2 bool
+	EnableH3  bool
+}
+
+func New(config ...Config) Conn {
+	if len(config) == 0 {
+		return newH2Conn()
+	}
+	if config[0].Proxy != "" || config[0].DisableH2 {
+		return newH1Conn(config[0].Proxy)
+	}
+	if config[0].EnableH3 {
+		return newH3Conn()
+	}
+	return newH2Conn()
+}
+
+func newH1Conn(proxy string) Conn {
+	transport := &http.Transport{
+		DialTLSContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			c, err := net.Dial(network, addr)
+			if err != nil {
+				return nil, err
+			}
+			uconn := utls.UClient(c, nil, utls.HelloRandomizedNoALPN)
+			colonPos := strings.LastIndex(addr, ":")
+			if colonPos == -1 {
+				colonPos = len(addr)
+			}
+			uconn.SetSNI(addr[:colonPos])
+			err = uconn.Handshake()
+			return uconn, err
+		},
+		MaxIdleConnsPerHost: 128,
+		MaxIdleConns:        128,
+		MaxConnsPerHost:     128,
+		ForceAttemptHTTP2:   false,
+		TLSNextProto:        make(map[string]func(authority string, c *tls.Conn) http.RoundTripper),
+	}
+	if proxy != "" {
+		u, err := url.Parse(proxy)
+		if err != nil {
+			panic(err)
+		}
+		transport.Proxy = http.ProxyURL(u)
+	}
+	return &conn{
+		transport: transport,
+	}
+}
+
+func newH2Conn() Conn {
+	transport := &http2.Transport{
+		DialTLSContext: func(ctx context.Context, network, addr string, cfg *tls.Config) (net.Conn, error) {
+			c, err := net.Dial(network, addr)
+			if err != nil {
+				return nil, err
+			}
+			uconn := utls.UClient(c, &utls.Config{NextProtos: cfg.NextProtos}, utls.HelloRandomized)
+			colonPos := strings.LastIndex(addr, ":")
+			if colonPos == -1 {
+				colonPos = len(addr)
+			}
+			uconn.SetSNI(addr[:colonPos])
+			err = uconn.Handshake()
+			return uconn, err
+		},
+		MaxHeaderListSize:          0xffffffff,
+		MaxReadFrameSize:           0x01000000,
+		MaxDecoderHeaderTableSize:  0x00001000,
+		MaxEncoderHeaderTableSize:  0x00001000,
+		StrictMaxConcurrentStreams: true,
+		IdleConnTimeout:            0,
+		ReadIdleTimeout:            1 * time.Second,
+		PingTimeout:                1 * time.Second,
+		WriteByteTimeout:           1 * time.Second,
+	}
+
+	return &conn{
+		transport: transport,
+	}
+}
+
+func newH3Conn() Conn {
+	transport := &http3.RoundTripper{
+		QUICConfig: &quic.Config{
+			HandshakeIdleTimeout: 1 * time.Second,
+			MaxIdleTimeout:       1 * time.Second,
+		},
+	}
+	return &conn{
+		transport: transport,
+	}
+}
+
+func (c *conn) RoundTrip(r *http.Request) (resp *http.Response, err error) {
+	return c.transport.RoundTrip(r)
+}
--- go.mod
+++ go.mod
@@ -4,15 +4,25 @@ go 1.22
 
 require (
 	github.com/mozillazg/go-httpheader v0.4.0
+	github.com/quic-go/quic-go v0.46.0
 	github.com/refraction-networking/utls v1.6.7
 	golang.org/x/net v0.28.0
 )
 
 require (
-	github.com/andybalholm/brotli v1.0.6 // indirect
-	github.com/cloudflare/circl v1.3.7 // indirect
-	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/cloudflare/circl v1.3.9 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/google/pprof v0.0.0-20240727154555-813a5fbdbec8 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/onsi/ginkgo/v2 v2.20.0 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	go.uber.org/mock v0.4.0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
+	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
 )
